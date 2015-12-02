ab9e1cfa1cd01dd813189b887145c596e34e6d44
Alex Sverdlov <alex@theparticle.com>
2015-12-02 08:13:17 -0500

    added hive variable/add-file support to depsql; every subsequently generated query will get a set of definitions

--- bin/depsql.pl
+++ bin/depsql.pl
@@ -19,35 +19,59 @@ map {my ($n,$v)=split/=/,$_,2; $args{$n}= defined($v) ? $v:1 } @ARGV;
 
 $args{prefix} = "" unless defined $args{prefix};
 
-die qq(usage: cat *sql | $0 
+die qq(usage: 
+        cat *sql | $0 gensql pdistfile=run.txt - 
+        $0 gensql pdistfile=run.txt file1.sql file2.sql file3.sql ...
+
     options: 
         gensql      if specified, generates a .sql file for each query.
         pdistfile   if specified, creates a file suitable to pipe into pdist.pl.
-    
+        -           dash specifies to read stdin.
+        
         debug       dump output to stdout.
-        prefix      output files have prefix: default is empty.        
+        prefix      output files have prefix: default is empty.
 ) if $args{help};
 
-# read input
-{ local $/=undef; $_=<STDIN>; }
+my @str;            # all strings
+my @stmts;          # all statements
 
-# apply environment variables.
-s/\$\{(\w+)\}/$ENV{$1}/sgie;
-s/\&\&?(\w+)/$ENV{$1}/sgie;
+# for everything on stdin, and all files on command line.
+for my $p (@ARGV){
+    $_ = undef;         # new input
+    if(-f $p){
+        open my $in,$p;    # if user gives us filename, then read that file.
+        next unless $p; # skip silently 
+        local $/=undef; # slurp mode
+        $_=<$in>;
+        close $in;      # close file.
+    }elsif($p eq '-'){
+        local $/=undef; 
+        $_=<STDIN>;   # if user specifies - at command line, read stdin.
+    }else{
+        next;           # skip this arg
+    }
 
-# save all strings
-my @str; 
-s/(--.*?)\n/push @str,$1; '#######'.$#str."#######\n"/sgie;
-s/('.*?')/push @str,$1; '#######'.$#str.'#######'/sgie;
+    next unless $_;    # skip unless read stuff
 
-# cut input into statements
-my @stmts = split/;/;       # cut into statements
-pop @stmts;     # remove stuff after last statement.
+    # apply environment variables.
+    s/\$\{(\w+)\}/$ENV{$1}/sgie;
+    s/\&\&?(\w+)/$ENV{$1}/sgie;
+
+    s/(--.*?)\n/push @str,$1; '#######'.$#str."#######\n"/sgie;
+    s/('.*?')/push @str,$1; '#######'.$#str.'#######'/sgie;
+
+    # cut input into statements
+    push @stmts,split/;/;       # cut into statements
+    $stmts[-1] = undef;         # remove stuff after last statement (indicate EOF)
+
+    # undef in statement list indicates end of file; variable get reset, etc.  
+}
 
 my @stmtdep = map { [] } @stmts;
 my (%usedtbls, %tbls);
 for(my $i=0;$i<=$#stmts;$i++){
     local $_ = $stmts[$i];
+    next unless $_;
     for my $t (keys %tbls){
         if(m/\b(\Q$t\E)\b/i){
             push @{$stmtdep[$i]},$tbls{$t};
@@ -77,22 +101,42 @@ if($args{pdistfile}){
     open $out,">$args{pdistfile}" or die $!;
 }
 
+
+my (%addfile,%setcmd);   # add file, set variable.
+
 for(my $i=0;$i<=$#stmts;$i++){
     local $_ = $stmts[$i];
-    s/^\s+|\s+$//sgi; 
+
+    unless(defined($_)){    # reached end of file, reset variables.
+        %addfile = ();
+        %setcmd = ();
+        next;
+    }
+    s/^\s+|\s+$//sgi;
     if($_){
         my %dups;
         my @depdedup = grep { ! $dups{$_}++ } @{$stmtdep[$i]};
         $_ = $stmts[$i];
         s/^\s+|\s+$//sgi;
-        s/#######(\d+)#######/$str[$1]/sgie; 
+        s/#######(\d+)#######/$str[$1]/sgie;
+
+        if(m/^\s*set\s+(\w+)/si){
+            $setcmd{$1} = $_;
+            next;
+        }elsif(m/^\s*add\s+file\s+([^\n]+)/si){
+            $addfile{$1} = $_;
+            next;
+        }
+
         if($args{debug}){
             print "-- query $i, depends on: ".join(", ",@depdedup)."\n";
+            print join("",sort map { "$_;\n" } (values %addfile,values %setcmd));
             print $_."\n;\n";
         }
         if($args{gensql}){
-            open my $out2,">$args{prefix}$i.sql" or die $!;    
+            open my $out2,">$args{prefix}$i.sql" or die $!;            
             print $out2 "-- query $i, depends on: ".join(", ",@depdedup)."\n";
+            print join("",sort map { "$_;\n" } (values %addfile,values %setcmd));
             print $out2 $_."\n;\n";
             close $out2;
         }
